0ac993d45710dcb569f10c1f01cd3a67ba417f37
Hugo <hcicarelli@gmail.com>
2018-12-10 11:13:45 -0200

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<meta property='og:site_name' content='LAHAR' />
+<meta property="fb:admins" content="100001037542130" />
+<meta property="fb:page_id" content="542161112559708" />
+<meta property="og:locale" content="pt_BR" />
+<meta property='og:type' content='website' />
+
+<meta name="twitter:card" content="summary"/>
+<meta name="twitter:description" content="LAHAR - Automação de Marketing, Landing Pages, E-mail Marketing, Ferramentas para mídias sociais e muito mais."/>
+<meta name="twitter:title" content="LAHAR - Software de Automação de Marketing e Inteligência em Vendas"/>
+<meta name="twitter:site" content="@lahardigital"/>
+<meta name="twitter:domain" content="LAHAR"/>
+<meta name="twitter:image:src" content="http://www.lahar.com.br/images/lahar.jpg"/>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-58463748-1'], ['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script');
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+
+<!-- Global site tag (gtag.js) - Google AdWords: 951909011 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-951909011"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-951909011');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1740282269593158');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1740282269593158&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+<link rel="stylesheet" type="text/css" href="css/responsivo.css">
---
+++ site2018/js/custom.js
@@ -0,0 +1,198 @@
+prices = {
+	"5" : {
+		"basic":{
+			"month":229,
+			"year":2290/12
+		},
+		"professional":{
+			"month":349,
+			"year":3490/12
+		},
+		"enterprise":{
+			"month":979,
+			"year":9790/12
+		},
+	},
+	"10" : {
+		"basic":{
+			"month":275,
+			"year":2750/12
+		},
+		"professional":{
+			"month":389,
+			"year":3890/12
+		},
+		"enterprise":{
+			"month":1159,
+			"year":11590/12
+		},
+	},
+	"25" : {
+		"basic":{
+			"month":349,
+			"year":3490/12
+		},
+		"professional":{
+			"month":518,
+			"year":5180/12
+		},
+		"enterprise":{
+			"month":1239,
+			"year":12390/12
+		},
+	},
+	"50" : {
+		"basic":{
+			"month":525,
+			"year":5250/12
+		},
+		"professional":{
+			"month":660,
+			"year":6600/12
+		},
+		"enterprise":{
+			"month":1509,
+			"year":15090/12
+		},
+	},
+	"75" : {
+		"basic":{
+			"month":755,
+			"year":7550/12
+		},
+		"professional":{
+			"month":970,
+			"year":9700/12
+		},
+		"enterprise":{
+			"month":1769,
+			"year":17690/12
+		},
+	},
+	"100" : {
+		"basic":{
+			"month":950,
+			"year":9500/12
+		},
+		"professional":{
+			"month":1199,
+			"year":11990/12
+		},
+		"enterprise":{
+			"month":1969,
+			"year":19690/12
+		},
+	},
+	"125" : {
+		"basic":{
+			"month":1120,
+			"year":11200/12
+		},
+		"professional":{
+			"month":1319,
+			"year":13190/12
+		},
+		"enterprise":{
+			"month":2149,
+			"year":21490/12
+		},
+	},
+	"150" : {
+		"basic":{
+			"month":1350,
+			"year":13500/12
+		},
+		"professional":{
+			"month":1519,
+			"year":15190/12
+		},
+		"enterprise":{
+			"month":2549,
+			"year":25490/12
+		},
+
+	},
+	"200" : {
+		"basic":{
+			"month":1729,
+			"year":17290/12
+		},
+		"professional":{
+			"month":1929,
+			"year":19290/12
+		},
+		"enterprise":{
+			"month":2999,
+			"year":29990/12
+		},
+	}
+}
+
+
+numberOfContacts = "5";
+paymentPeriod = "month"
+plans = [
+	"basic",
+	"professional",
+	"enterprise"
+];
+
+function changeNumberOfContacts(e) {
+	for (key in prices) {
+		document.getElementById("contact"+key+"button").classList.remove("active");
+	}
+	e.target.classList.add("active");
+	numberOfContacts = e.target.getAttribute("data-value");
+	changePrice();
+}
+
+function changePaymentPeriod(e) {
+	if (e.target.getAttribute("data-value")) {
+		document.getElementById("paymentPeriodmonthButton").classList.remove("active");
+		document.getElementById("paymentPeriodyearButton").classList.remove("active");
+		e.target.parentNode.classList.add("active");
+
+		paymentPeriod = e.target.getAttribute("data-value");
+
+		for(j of document.querySelectorAll(".monthEquivalent")){
+			j.style.display = paymentPeriod == "month" ? "none" : "block";
+		}
+
+		changePrice();
+	}
+}
+
+function changePrice() {
+	for (plan of plans) {
+		document.getElementById("planPrice-"+plan).innerHTML = Math.ceil(prices[numberOfContacts][plan][paymentPeriod]);
+		document.getElementById('equivalentPrice-month-'+plan).innerHTML = prices[numberOfContacts][plan][paymentPeriod] * 12
+	}
+
+}
+
+function arrowFoward() {
+	if(document.querySelector('.w-tab-link.w--current').nextElementSibling.classList && document.querySelector('.w-tab-link.w--current').nextElementSibling.classList.contains('tab-link')) {
+		document.querySelector('.w-tab-link.w--current').nextElementSibling.click()
+	}
+}
+
+function arrowBackwards() {
+	if(document.querySelector('.w-tab-link.w--current').previousElementSibling.classList && document.querySelector('.w-tab-link.w--current').previousElementSibling.classList.contains('tab-link')) {
+		document.querySelector('.w-tab-link.w--current').previousElementSibling.click()
+	}
+}
+
+function changeLanguage(lng) {
+	var url =  window.location.toString().replace(/\/en\/|\/es\//, '/');
+	window.location = url.replace(/(\/[\w.-]+?)$/, (lng ? "/"+lng : '')+"$1");
+}
+
+$(document).ready(function(){
+	var hBanner = $(".section-hero").height();
+	var offPage = $(document).scrollTop();
+
+	if((0.9*offPage) >= hBanner)
+		$(".nav-off").css({"opacity":"1","display":"block","transform": "translate(0px, 0px)"})
+});
+
+
